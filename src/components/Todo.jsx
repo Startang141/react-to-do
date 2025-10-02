9ca7c7789cd1272a4cfcd0c58f9ae605a70d84ae
startang141 <bintangadiyatma141@gmail.com>
2025-10-02 21:51:45 +0700

    feat: add success, delete error

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReportBox from "./ReportBox";
 import TodoItems from "./TodoItems";
 
 function Todo() {
+  const [ToDoList, setToDoList] = useState([]);
+  const inputRef = useRef();
+
+  const addToDo = () => {
+    const inputToDo = inputRef.current.value.trim();
+    if (inputToDo === "") {
+      return null;
+    }
+
+    const newToDo = {
+      id: Date.now(),
+      ToDo: inputToDo,
+      isComplete: false,
+    };
+
+    setToDoList((prev) => [...prev, newToDo]);
+  };
+
+  const deleteToDo = (id) => {
+    setToDoList((prevToDo) => {
+      return prevToDo.filter((ToDo) => ToDo.id !== id);
+    });
+  };
   return (
-    <div className="bg-white md:w-2/5 lg:w-1/3 h-5/6 place-self-center rounded-2xl p-4 overflow-hidden">
+    <div className="bg-white md:w-2/5 lg:w- h-5/6 place-self-center rounded-2xl p-4 overflow-hidden">
       {/* Heading */}
       <h1 className="text-2xl font-semibold text-gray-700">To Do List 🐔</h1>
       <p className="text-sm text-gray-600">List Now, Doing One Day</p>
@@ -19,20 +42,32 @@ function Todo() {
       {/* Input */}
       <div className="bg-slate-100 flex items-center rounded-md">
         <input
+          ref={inputRef}
           className="rounded-md w-full py-3 px-2"
           placeholder="To Do"
           type="text"
         />
-        <button className="bg-gray-900 text-white px-4 py-3 rounded-tr-md rounded-br-md cursor-pointer">
+        <button
+          onClick={addToDo}
+          className="bg-gray-900 text-white px-4 py-3 rounded-tr-md rounded-br-md cursor-pointer"
+        >
           Add
         </button>
       </div>
 
       {/* List To Do */}
       <div className="overflow-y-auto h-1/2 my-4">
-        <TodoItems doing="" />
-        <TodoItems doing="" />
-        <TodoItems doing="" />
+        {ToDoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              doing={item.ToDo}
+              id={item.id}
+              isComplete={item.isComplete}
+              deteleToDo={deleteToDo}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,14 +1,20 @@
 import { BadgeCheck, Trash2 } from "lucide-react";
 import React from "react";
 
-function TodoItems({ doing }) {
+function TodoItems({ doing, id, isComplete, deleteToDo }) {
   return (
     <div className="flex items-center justify-between my-4 border rounded-md py-4 px-2 hover:bg-slate-50">
       <div className="flex items-center gap-2">
         <BadgeCheck />
         <p>{doing}</p>
       </div>
-      <Trash2 className="cursor-pointer" />
+      <button
+        onClick={() => {
+          deleteToDo(id);
+        }}
+      >
+        <Trash2 className="cursor-pointer" />
+      </button>
     </div>
   );
 }
